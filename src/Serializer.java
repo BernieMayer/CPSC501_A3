bbfc828191b98199a24828cc0f6195675ead7772
Bdog94 <berniemayer94@gmail.com>
2016-11-06 22:25:51 -0700

    Started working on serializing primitives

--- src/Serializer.java
+++ src/Serializer.java
@@ -1,10 +1,11 @@
 import org.jdom2.*;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class Serializer {
 	
-	public org.jdom2.Document serialize(Object obj)
+	public org.jdom2.Document serialize(Object obj) throws IllegalArgumentException, IllegalAccessException
 	{
 		Element baseElement = new Element("serialized");
 		Document d = new Document(baseElement);
@@ -22,9 +23,52 @@ public class Serializer {
 		objectTag.setAttributes(attribs);
 		d.getRootElement().addContent(objectTag);
 		
+		if (obj.getClass().isArray())
+		{
+			for (Field aField:obj.getClass().getFields())
+			{
+				if (aField.getClass().isPrimitive())
+				{
+					String value = aField.get(obj).toString();
+					
+				    Element fieldElem = new Element("field");
+				    Attribute nameAttrib = new Attribute("name", aField.getName());
+				    fieldElem.setAttribute(nameAttrib);
+				    
+				    
+				    Attribute declaringClass = new Attribute("declaringclass", obj.getClass().getSimpleName());
+				    fieldElem.setAttribute(declaringClass);
+				    
+				    Element valElem = new Element("value");
+				    valElem.setText(aField.get(obj).toString());
+				    
+				    fieldElem.addContent(valElem);
+				    
+				    
+				    objectTag.addContent(fieldElem);
+				    
+					
+					
+					
+				} else 
+				{
+					//handle object references here
+				}
+			}
+			
+		} else 
+		{
+			//handle arrays here
+			
+		}
 		
 		return d;
 	}
+	
+	
+
+	
+	
 	
 	
 	
